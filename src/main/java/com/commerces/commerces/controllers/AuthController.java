6def9fc286827a032ff550f92178877b2bc9adc0
Anniymm <animaisuradze1999@gmail.com>
2025-02-14 22:06:18 +0400

    change main url REQUESTMAPPING

--- src/main/java/com/commerces/commerces/controllers/AuthController.java
+++ src/main/java/com/commerces/commerces/controllers/AuthController.java
@@ -23,7 +23,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/auth")
+@RequestMapping("/auths")
 public class AuthController {
 
     private final AuthenticationManager authenticationManager;
