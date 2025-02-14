df5ad332e5a5783786d863742fa9b147a81434a7
Anniymm <animaisuradze1999@gmail.com>
2025-02-14 21:03:35 +0400

    initial project starting commit

---
+++ src/test/java/com/commerces/commerces/CommercesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.commerces.commerces;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommercesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
