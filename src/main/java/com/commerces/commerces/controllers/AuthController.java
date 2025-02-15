588f0821abee32412dc15122d185a1c8ac0742cc
Anniymm <animaisuradze1999@gmail.com>
2025-02-15 18:25:18 +0400

    version of full jwt from this point of view

--- src/main/java/com/commerces/commerces/controllers/AuthController.java
+++ src/main/java/com/commerces/commerces/controllers/AuthController.java
@@ -23,7 +23,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/auths")
+@RequestMapping("/auth")
 public class AuthController {
 
     private final AuthenticationManager authenticationManager;
