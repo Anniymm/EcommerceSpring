5a7542dd6c169a51d38c072073970881b5285e27
Anniymm <animaisuradze1999@gmail.com>
2025-02-22 21:17:45 +0400

    add basket and basketitem models

---
+++ src/main/java/com/commerces/commerces/models/Basket.java
@@ -0,0 +1,31 @@
+package com.commerces.commerces.models;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Setter
+@Getter
+@Entity
+@Table(name="baskets")
+public class Basket {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @OneToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user; //anu kalata konkretul users ekutvnis
+    // onetoone imistvis rom ert users erti kalata aqvs
+
+    @OneToMany(mappedBy = "basket", cascade = CascadeType.ALL)
+    //erti cali basketi sheicavs sxvadasva products amitom minda onetomany
+    private List<Product> products;
+
+
+    //no-argument (default) constructor
+    // radgan relationebi maqvs avtomaturad ver igulismeba es amitom unda davuwero (magalitad Product-shi avtomaturad moiazreba)
+    public Basket() {}
+}
---
+++ src/main/java/com/commerces/commerces/models/BasketItem.java
@@ -0,0 +1,33 @@
+package com.commerces.commerces.models;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+@Entity
+@Table(name = "basket_items")
+public class BasketItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "basket_id", nullable = false)
+    private Basket basket;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id", nullable = false)
+    //    Many BasketItem entries can be associated with one Product.
+//    Each BasketItem must reference a single Product via the foreign key (product_id).
+//    A single product can exist in multiple BasketItem records if different baskets contain the same product.
+    private Product product;
+
+    private int quantity;
+
+    private double totalPrice;
+
+    public BasketItem() {}
+
+}
